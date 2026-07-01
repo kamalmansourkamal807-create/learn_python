9dd3b9e535abffb11a53867222aedd7fe25a99a4
kamalmansourkamal807-create <kamalmansourkamal807@gmail.com>
2026-07-01 12:43:19 +0300

    Update part_1.py

--- part_1.py
+++ part_1.py
@@ -4,4 +4,15 @@ y = 10
 z = 8
 print(x > y)
 print(y > z)
- 
+#Ans
+ # False
+ # True
+             """"############HHHHHHHHHHHHHH"""
+#Question 2: What is the output of the following snippet?
+x, y, z = 5, 10, 8
+print(x > z)
+print((y - 5) == x)
+# Ans
+# False
+# True
+             """"############HHHHHHHHHHHHHH"""
